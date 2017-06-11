5e3b0c940b3bd2a1fc1559921b86eae3f2bf17a2
Jake Daryl Moresca <JakeDaryl.Moresca@infor.com>
2017-06-11 13:29:49 +0800

    Initial Project files

---
+++ Information Management/Moresca.IM.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Moresca.IM.Web.MvcApplication" Language="C#" %>
---
+++ Information Management/Moresca.IM.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Moresca.IM.Web.Startup))]
+
+namespace Moresca.IM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
